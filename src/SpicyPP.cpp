994bab1ffdc7fe88c4445740a3d183e7f0350673
MariusBaumann <marius.baumann@ost.ch>
2020-11-04 08:55:59 -0800

    First Commit as Test

---
+++ src/SpicyPP.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : SpicyPP.cpp
+// Author      : Marius Baumann
+// Version     :
+// Copyright   : (c) by Marius Baumann
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
