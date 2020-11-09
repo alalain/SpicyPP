b769b6edb4f07501f26d23675ba9cfd8d7906d64
JODBaer <JODBaer@github.com>
2020-11-09 23:39:28 +0100

    Update Matrix.cpp
    Einheitsmatrix an original angefügt. (not tested)

--- src/Backend/Matrix.cpp
+++ src/Backend/Matrix.cpp
@@ -37,7 +37,38 @@ Matrix::Matrix(const Matrix &other)
   // TODO Auto-generated constructor stub
 
 }
+Matrix getInverse(Matrix& A)
+{
+	double pivot[A.rows] = 0;
+	int row = 0;
+	int colum = 0;
+	Matrix invmatrix(A.rows,2*A.colums);
+	
+	// Eigenmatrize added
+	for(int row = A.rows; row < A.rows*2; row++) {
+	    for(int colum = A.colums; colum < 2*A.colums; colum++){
+		  if(row== colum)
+			 inmvmatrix[row][colum] = 1.0;
+		  else
+			 inmvmatrix[row][colum] = 0.0;
+        }
+    }
+	
+	pivot[row]=A.matrix[0][0];
+	/*
+	  for(int row = 0; row < 2*A.rows; row++) {
+		for(int colum = 0; colum < 2*A.colums; colum++){
+			invmatrix[row][colum] = A.matrix[row][colum]/pivot[row];
+		}
+		for(int colum = 0; colum < 2*A.colums; colum++){
+			invmatrix[row][colum] = A.matrix[row][colum]/pivot[row];
+		}
 
+		
+	}
+	*/
+	return invmatrix
+}
 Matrix& Matrix::operator=(const Matrix &other)
 {
   // TODO Auto-generated method stub
