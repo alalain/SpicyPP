06a89920ed7e64e1a3a2271be126d9e4aba8c92b
Alain Keller <a1keller@hsr.ch>
2020-11-10 15:06:05 +0100

    added empty mainWindow

--- src/SpicyPP.cpp
+++ src/SpicyPP.cpp
@@ -6,12 +6,19 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+#include <QApplication>
 #include <iostream>
+#include "MainWindow.h"
 
 #include "Backend/Matrix.h"
 
 using namespace std;
 
-int main(int argc, char** argv) {
-	return 0;
+int main(int argc, char** argv)
+{
+  QApplication a(argc, argv);
+  MainWindow w;
+  w.show();
+
+  return a.exec();
 }
