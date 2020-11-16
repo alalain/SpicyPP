6f4d0b1401b147d0037d108d8e2c322f443f1be9
Alain <mceagle117@gmail.com>
2020-11-16 08:38:49 +0100

    Revert "Merge branch 'Developement' of https://github.com/LordMcFungus/SpicyPP"
    This reverts commit ecf67da80f97883c142e8289117a0d22636818ce, reversing
    changes made to 43462659352f13c6b1b82b28f70b343774aae62e.

--- src/Backend/Matrix.cpp
+++ src/Backend/Matrix.cpp
@@ -6,26 +6,10 @@
  */
 
 #include "Matrix.h"
+#include <stdexcept>
 
 using namespace std;
 
-// Backend main
-/*
-int main(){
-  vector<double> v = {1.0,2.0,3.0, 3.0, 5.0, 6.0, 7.0, 8.0, 4.0};
-  Matrix m1 = Matrix(2,2,v);
-  m1.MatrixShow();
-  Matrix m2 = Matrix(3,3,v);
-
-  m2.MatrixShow();
-  Matrix m3 = m2.getInverse();
-
-  m2.MatrixShow();
-  m3.MatrixShow();
-
-  return 0;
-}
-*/
 Matrix::Matrix(int rows, int columns, vector<double> values)
 {
   this->values = vector<vector<double>>();
@@ -37,7 +21,7 @@ Matrix::Matrix(int rows, int columns, vector<double> values)
   for(int row = 0; row < rows; row++) {
     this->values.push_back(vector<double>());
     for(int column = 0; column < columns; column++){
-      unsigned int index = row * columns + column;
+      int index = row * columns + column;
       if(index >= values.size())
         this->values[row].push_back(0.0);
       else
@@ -47,78 +31,139 @@ Matrix::Matrix(int rows, int columns, vector<double> values)
 }
 
 
-Matrix Matrix::getInverse() const
+Matrix getInverse(Matrix& A)
 {
-	double pivot[values.size()] = {};
 
-	int rows = values.size();
-	int colums = values[0].size();  // 1. Row because every Colum have equal size.
-	int rreduce = 0;
-
-	Matrix bigmatrix(rows,2*colums);
-	Matrix result(rows,colums,{});
-	bigmatrix.values= values;     //set equal, copy,
-
+	double pivot = 0;
+	/*
+	Matrix bigmatrix(A.rows,2*A.colums);
+	Matrix result(A.rows,A.colums);
+	
 	// Eigenmatrize added
-	for(int row = 0; row < rows; row++) {
-
-	    bigmatrix.values[row].resize(rows+colums);  //because of the set equal the bigmatrix was shrinked.
-
-	    for(int colum = colums; colum < 2*colums; colum++){
-		  if(row == (colum-colums))
-			 bigmatrix.values[row][colum] = 1.0;
+	for(int row = A.rows; row < A.rows; row++) {
+	    for(int colum = A.colums; colum < 2*A.colums; colum++){
+		  if(row== (colum-A.colums))
+			 bigmatrix[row][colum] = 1.0;
 		  else
-			 bigmatrix.values[row][colum] = 0.0;
+			 bigmatrix[row][colum] = 0.0;
       }
-  }
-
-	//TODO delet after tested
-	cout<< "Einheitsvektor"<<endl;
-	bigmatrix.MatrixShow();
-
-	for(int row = 0; row < rows; row++) {
-		if( bigmatrix.values[row][row] != 0){
-
-			pivot[row]=bigmatrix.values[row][row];
+   }
+	
+	for(int row = 0; row < A.rows; row++) {
+		if( A.matrix[row][row] != 0){
+			pivot[row]=A.matrix[row][row];
 		
-			for(int colum = 0; colum < 2*colums; colum++){
-				bigmatrix.values[row][colum] = bigmatrix.values[row][colum]/pivot[row];  // pivot should not be zero
+			for(int colum = 0; colum < 2*A.colums; colum++){
+				bigmatrix[row][colum] = A.matrix[row][colum]/pivot[row];  // pivot shold not be zero
+			}
+			for(int rreduce = row+1; rreduce < A.rows; rreduce++) {
+				for(int colum = 0; colum < 2*A.colums; colum++){
+					bigmatrix[rreduce][colum] = bigmatrix[rreduce][colum] - bigmatrix[rreduce][row]*bigmatrix[row][colum]; // seconde row - second row faktor* first row
+				}
 			}
 
-			//TODO delet after tested
-			cout<< "Pivot Teilung"<<endl;
-			bigmatrix.MatrixShow();
-
-
-
-			for(rreduce = 0; rreduce < rows; rreduce++) {
-			  if(rreduce != row){
-
-			    double ratio = bigmatrix.values[rreduce][row] / bigmatrix.values[row][row];
-
-          for(int colum = 0; colum < 2*colums; colum++){
-            bigmatrix.values[rreduce][colum] -=  (bigmatrix.values[row][colum] * ratio);
-          }
-			  }
-			}
-
-			//TODO delet after tested
-			cout<< "Reihen reduzierung"<<endl;
-			bigmatrix.MatrixShow();
-
 		}
-		else{		// shit pivot was 0 ???
-
-		  throw range_error("Inverse doesn't exist because the Matrix is singular");
-		}
+		else		// shit pivot was 0 ???
 	}
-
-	for (int i=0; i< rows; i++){
-	  for(int j=0; j<colums; j++){
-	    result.values[i][j]=bigmatrix.values[i][j+colums];
-	  }
-
-	}
-	return result;
+	
+	
+	return invmatrix*/
+	return Matrix(0,0);
 }
+
+
+void Matrix::setValue(double value, int row, int column)
+{
+  if(row <= 0 || row > values.size() || column <= 0 || column > values[0].size())
+    return;
+  values[row-1][column-1] = value;
+}
+
+void Matrix::setValueColumn(std::vector<double> inputValues, int column)
+{
+  int sizeOfInputValues = inputValues.size();
+    int sizeOfValues = values.size();
+
+    for(int i = 1; i <= sizeOfValues; ++i)
+    {
+      if(i <= sizeOfInputValues)
+      {
+        setValue(inputValues[i-1], i, column);
+      }
+    }
+}
+
+void Matrix::setValueColumn(std::vector<double> inputValues, int column, double fillValue)
+{
+  int sizeOfInputValues = inputValues.size();
+  int sizeOfValues = values.size();
+
+  for(int i = 1; i <= sizeOfValues; ++i)
+  {
+    if(i <= sizeOfInputValues)
+    {
+      setValue(inputValues[i-1], i, column);
+    }
+    else
+    {
+      setValue(fillValue ,i, column);
+    }
+  }
+}
+
+void Matrix::setValueRow(std::vector<double> inputValues, int row)
+{
+  int sizeOfInputValues = inputValues.size();
+    int sizeOfValues = values.size();
+
+    for(int i = 1; i <= sizeOfValues; ++i)
+    {
+      if(i <= sizeOfInputValues)
+      {
+        setValue(inputValues[i-1], i, row);
+      }
+    }
+}
+
+void Matrix::setValueRow(std::vector<double> inputValues, int row, double fillValue)
+{
+  int sizeOfInputValues = inputValues.size();
+  int sizeOfValues = values.size();
+
+  for(int i = 1; i <= sizeOfValues; ++i)
+  {
+    if(i <= sizeOfInputValues)
+    {
+      setValue(inputValues[i-1], i, row);
+    }
+    else
+    {
+      setValue(fillValue ,i, row);
+    }
+  }
+}
+
+Matrix operator*(const Matrix& left, const Matrix& right)
+{
+  if(left.values[0].size() == right.values.size())
+  {
+    Matrix matrixProduct = Matrix(left.values.size(), right.values[0].size());
+    for(unsigned int r = 1; r <= matrixProduct.values.size(); ++r)
+    {
+      for(unsigned int c = 1; c <= matrixProduct.values[0].size(); ++c)
+      {
+        for(unsigned int elementIndex = 1; elementIndex <= left.values.size(); ++elementIndex)
+        {
+          matrixProduct.setValue(left.values[r-1][elementIndex-1]*right.values[elementIndex-1][c-1] + matrixProduct.values[r-1][c-1], r, c);
+        }
+      }
+    }
+    return matrixProduct;
+  }
+  else
+  {
+    throw range_error("Incorrect dimension for matrix multiplication");
+  }
+}
+
 
