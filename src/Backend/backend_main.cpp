2e7b068c107c1f8c9b4e3f625d73c43381f15958
user <marius.baumann@ost.ch>
2020-11-18 12:53:31 -0800

    Rest of Commits so that i can pull and merge

--- src/Backend/backend_main.cpp
+++ src/Backend/backend_main.cpp
@@ -9,18 +9,24 @@
 
 using namespace std;
 
+
+#include <set>
 int main()
 {
   Network net;
   net.AddResistor("R1", 0, 1, 10);
-  net.AddResistor("R2", 0, 2, 15);
-  net.AddResistor("R3", 1, 2, 20);
+  net.AddResistor("R2", 2, 3, 15);
   net.AddVoltageSource("V1", 0, 1, 5);
 
-  vector<string> netList = net.GetNetlist();
+  vector<string> netList = net.GetNewestNetlist();
   cout << "NetList:" << endl;
   for(unsigned int i = 0; i < netList.size(); ++i)
     cout << netList[i] << endl;
+
+  net.GetNewestSolution();
+  Matrix netMatrix = net.GetCurrentNetMatrix();
+  netMatrix.MatrixShow();
+
   return 0;
 }
 
