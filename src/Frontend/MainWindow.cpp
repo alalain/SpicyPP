9fa558c2802d107247d863eab7ab96310d08f499
user <thierry.schwaller@hsr.ch>
2020-11-18 05:51:50 -0800

    Implemented input error messages

--- src/Frontend/MainWindow.cpp
+++ src/Frontend/MainWindow.cpp
@@ -12,7 +12,10 @@ MainWindow::MainWindow(QWidget* parent)
 
   connect(ui->ResetButton, &QPushButton::clicked, this, &MainWindow::onReset);
   connect(ui->AddButton, &QPushButton::clicked, this, &MainWindow::onAdd);
-  ui->Node1->setValidator(new QIntValidator(0, 100, this));
+  ui->Node1->setValidator(node1Validator);
+  ui->Node2->setValidator(node2Validator);
+  ui->Value->setValidator(valueValidator);
+  ui->Component->setValidator(componentValidator);
 }
 
 MainWindow::~MainWindow()
@@ -27,9 +30,38 @@ void MainWindow::onReset()
 void MainWindow::onAdd()
 {
   // Resistor = 0 VoltageSource = 1
+  int pos = 0;
+  bool errorDetected = false;
+  QString node1String = ui->Node1->text();
+  QString node2String = ui->Node2->text();
+  QString valueString = ui->Value->text();
   int componentIndex = ui->Component->currentIndex();
-
   QString errorMessage = "";
+
+  if ((ui->Node1->validator()->validate(node1String, pos)) != 2)
+  {
+    errorDetected = true;
+    errorMessage.append("Node1 muss zwischen 0 und 100 liegen \n");
+  }
+
+  if ((ui->Node2->validator()->validate(node2String, pos)) != 2)
+  {
+    errorDetected = true;
+    errorMessage.append("Node2 muss zwischen 0 und 100 liegen \n");
+  }
+
+  if ((ui->Value->validator()->validate(valueString, pos)) != 2)
+  {
+    errorDetected = true;
+    errorMessage.append("Wert bitte anderst wählen \n");
+  }
+  if (errorDetected)
+  {
+    ui->ErrorLabel->show();
+    ui->ErrorLabel->setText(errorMessage);
+    return;
+  }
+  ui->ErrorLabel->hide();
   if (componentIndex == 0)
   {
   }
